7dbbc55ee098d1ebc5856106fdad46b9085306b3
Malimbita3994 <japhetfaustine39@gmail.com>
2026-03-19 09:00:55 +0300

    App Update

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,14 @@
 import axios from 'axios';
+
 window.axios = axios;
 
+const API_HOST = 'http://10.0.2.2:8000';
+window.axios.defaults.baseURL = `${API_HOST}/api`;
+
+// Ensure JSON responses are expected
+window.axios.defaults.headers.common['Accept'] = 'application/json';
+
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+// Send cookies (for Sanctum authentication)
+window.axios.defaults.withCredentials = true;
